c161b1fa31f9e40c5ec10570a1c6b637ee936acd
Ethan McDowell <ethan.r.mcdowell@gmail.com>
2020-11-16 18:31:24 -0500

    validation completed for all necessary questions, testing all passed

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+const render = require("./lib/htmlRenderer");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -8,55 +9,68 @@ const fs = require("fs");
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
-const render = require("./lib/htmlRenderer");
+// UI variables
+var ui = new inquirer.ui.BottomBar();
+ui.log.write("__________WELCOME TO THE TEAM CREATOR__________");
 
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
+// employees array which will contain each employee that the user creates
 let employees = [];
 
+// three constants which hold the logic for validating inquirer questions
+// validates input to ensure only letters were enteres
+const validateName = (input) => {
+    if (/\d/.test(input) || input === ""){
+        return "Please enter a valid response using only letters.";
+    } else {
+    return true;
+    }
+}
+// validates input to ensure only numbers are enteres
+const validateId = (input) => {
+    if (isNaN(input) || input === ""){
+        return "Please enter a number.";
+    } else {
+        return true;
+    }
+}
+// validates input to ensure that an e-mail was entered
+const validateEmail = (input) => {
+    if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/.test(input)){
+        return true;
+    } else {
+    return "Please enter a valid e-mail address";
+    }
+}
+
+// constant holding the initial set of questions for the user to enter the manager's information
 const managerQ = [{
     type: "input",
     name: "name",
     message: "Please enter the manager's name.",
-    validate: (input) => {
-        if (/\d/.test(input) || input === ""){
-            return "Please enter this employee's name";
-        } else {
-        return true;
-        }}
+    validate: validateName
 }, {
     type: "input",
     name: "id",
     message: "Enter the manager's ID.",
-    validate: (input) => {
-        if (/\D/.test(input) || input === ""){
-            return "Please enter a number";
-        } else {
-            return true;
-        }}
+    validate: validateId
 }, {
     type: "input",
     name: "email",
     message: "Enter their e-mail address.",
-    validation: (input) => {
-        if (/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/.test(input)){
-            return true;
-        }
-        return "Please enter a valid e-mail address";
-        }
+    validate: validateEmail
 }, {
     type: "input",
     name: "officeNumber",
-    message: "Enter their office number."
+    message: "Enter their office number.",
+    validate: validateId
 }, {
     type: "list",
     name: "addTeam",
     message: "Would you like to add team members?",
     choices: ["Yes", "No"]
 }]
-
+// constant holding the set of questions for additional employees - intern and engineer
+// uses when statement to ask specific questions based on the employee's role
 const employeeQ = [{
     type: "list",
     name: "role",
@@ -65,25 +79,30 @@ const employeeQ = [{
 }, {
     type: "input",
     name: "name",
-    message: "Enter this employee's name."
+    message: "Enter this employee's name.",
+    validate: validateName
 }, {
     type: "input",
     name: "id",
-    message: "Enter the employee's ID."
+    message: "Enter the employee's ID.",
+    validate: validateId
 }, {
     type: "input",
     name: "email",
-    message: "Enter this employee's e-mail address."
+    message: "Enter this employee's e-mail address.",
+    validate: validateEmail
 }, {
     when: (answers) => answers.role === "Intern",
     type: "input",
     name: "school",
-    message: "Enter this employee's school."
+    message: "Enter this employee's school.",
+    validate: validateName
 }, {
     when: (answers) => answers.role === "Engineer",
     type: "input",
     name: "github",
-    message: "Please enter this employee's GitHub user name."
+    message: "Please enter this employee's GitHub user name.",
+    validate: validateName
 },{
     type: "list",
     name: "addTeam",
@@ -91,6 +110,9 @@ const employeeQ = [{
     choices: ["Yes", "No"]
 }];
 
+// init function prompts manager questions, then creates a new Manager object using user input data
+// if user wanted to add an additional team members, it runs the employeeList function, else it runs
+// the createDoc function.
 function init(){
     inquirer.prompt(managerQ)
     .then(managerStats => {
@@ -99,12 +121,13 @@ function init(){
         if(managerStats.addTeam === "Yes"){
             employeeList();
         } else{
-            console.log(employees);
             createDoc();
         }
     });
 }
-
+// prompts the additional employee questions and creates a new Intern or Engineer based on the user input
+// and will add them to the team array, will loop back through questions if user chooses to do so, otherwise
+// will run the createDoc function. 
 function employeeList(){
     inquirer.prompt(employeeQ)
     .then(employeeStats => {
@@ -118,7 +141,6 @@ function employeeList(){
         if(employeeStats.addTeam === "Yes"){
             employeeList();
         } else {
-            console.log(employees);
             createDoc();
         }
     })
@@ -126,27 +148,7 @@ function employeeList(){
 
 init();
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
 function createDoc(){
     fs.writeFileSync(outputPath, render(employees), "utf-8");
     console.log("Success! Your page has been created.");
 }
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
